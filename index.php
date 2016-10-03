474697e50ce73aa7cc1cbb6e0e51dbda99621c03
bamsarts <bambanksupriadi@gmail.com>
2016-10-03 22:46:32 +0700

    versi 1.0.0

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>VPN-Premium</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="index.php">VPN Premium</a></h1>
+        </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+      <!-- Right Nav Section -->
+        <ul class="right">
+          <li><a href="about.php">About</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          
+        </ul>
+      </section>
+    </nav>
+
+
+
+
+    <img data-interchange="[images/bolt3.jpg, (large)], [images/bolt3.jpg, (mobile)], [images/bolt3.jpg, (medium)]">
+    <noscript><img src="images/bolt3.jpg"></noscript>
+
+
+    <div class="row" style="margin-top:10px;">
+      <div class="small-12">
+
+        <footer style="margin-top:10px;">
+           <p style="text-align:center; font-size:0.8em;">Powered by <a href="http://bamsarts.id" target="_blank" >bamsarts.id</p>
+        </footer>
+
+      </div>
+    </div>
+
+
+
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
